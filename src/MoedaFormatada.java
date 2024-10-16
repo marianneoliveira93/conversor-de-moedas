02b155da8f0d5a96bc7156daf7cc9c4276f274f3
Marianne Firmino <39701279+marianneoliveira93@users.noreply.github.com>
2024-10-16 16:10:31 -0300

    Update MoedaFormatada.java

--- src/MoedaFormatada.java
+++ src/MoedaFormatada.java
@@ -1,6 +1,9 @@
 package main.com.conversormoedas.api;
+// Declaração do pacote onde a classe está localizada
 
 
 public record MoedaFormatada(double conversion_rate) {
 
 }
+//Por "record", o Java cria automaticamente o construtor, os métodos getter, equals, hashCode e toString.
+// Declaração de uma classe de registro (record) chamada "MoedaFormatada", com a taxa de conversão (conversion_rate).
