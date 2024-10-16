fc807760d63e65e53d1979c473262ba1bf91467c
Marianne Firmino <39701279+marianneoliveira93@users.noreply.github.com>
2024-10-16 16:04:18 -0300

    Update ConverdorDeMoeda.java

--- src/ConverdorDeMoeda.java
+++ src/ConverdorDeMoeda.java
@@ -3,13 +3,13 @@ import java.util.Map;
 public class ConverdorDeMoeda {
 
 
-    // Método para converter uma quantia de uma moeda para outra
+    // Método utilizado para converter um valor de uma moeda para outra
     public static double convertCurrency(double amount, String fromCurrency, String toCurrency, Map<String, Double> rates) {
         if (!rates.containsKey(fromCurrency) || !rates.containsKey(toCurrency)) {
             throw new IllegalArgumentException("Moeda não suportada");
         }
 
-        // Calculando o valor em relação à moeda base (USD, por exemplo)
+        // Calculando o valor em relação a base
         double amountInBaseCurrency = amount / rates.get(fromCurrency);
         // Convertendo para a moeda desejada
         return amountInBaseCurrency * rates.get(toCurrency);
